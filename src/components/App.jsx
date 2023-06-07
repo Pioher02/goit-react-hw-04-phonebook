72f96374f41e3f70a729645724c3e0a4dbff8575
Pioher02 <dpaola.hernandezr@gmail.com>
2023-06-06 22:40:12 -0500

    Refactorizado App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,39 @@
-import { Component } from 'react';
 import ContactList from './ContactList';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import { nanoid } from 'nanoid';
 import Notiflix from 'notiflix';
+import { useEffect, useState } from 'react';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [prevContacts, setPrevContacts] = useState(contacts);
+  const [filter, setFilter] = useState('');
 
-  saveContact = evt => {
+  const saveContact = evt => {
     evt.preventDefault();
 
     const form = evt.currentTarget;
 
-    let validation = this.state.contacts.find(el =>
+    let validation = contacts.find(el =>
       el.name
         .toLocaleLowerCase()
         .includes(form.elements.name.value.toLocaleLowerCase())
     );
     if (validation === undefined) {
-      this.setState({
-        contacts: [
-          ...this.state.contacts,
-          {
-            id: nanoid(),
-            name: form.elements.name.value,
-            number: form.elements.number.value,
-          },
-        ],
-      });
+      setContacts([
+        ...contacts,
+        {
+          id: nanoid(),
+          name: form.elements.name.value,
+          number: form.elements.number.value,
+        },
+      ]);
       form.reset();
     } else {
       Notiflix.Notify.failure(validation.name + 'is already in contacts');
@@ -44,48 +41,45 @@ class App extends Component {
     }
   };
 
-  filterContact = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const filterContact = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  handleDeleteContacts = id => {
-    let updateContacts = this.state.contacts.filter(
-      contact => contact.id !== id
-    );
-    this.setState({ contacts: updateContacts });
+  const handleDeleteContacts = id => {
+    let updateContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(updateContacts);
   };
 
-  componentDidMount() {
+  useEffect(() => {
     const savedContacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(savedContacts);
 
     if (parsedContacts !== null) {
-      this.setState({ contacts: parsedContacts });
+      setContacts(parsedContacts);
     } else {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      localStorage.setItem('contacts', JSON.stringify(contacts));
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  useEffect(() => {
+    if (contacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+      setPrevContacts(contacts);
     }
-  }
+  }, [contacts]);
 
-  render() {
-    return (
-      <>
-        <ContactForm saveContact={this.saveContact}></ContactForm>
-        <h1>Contacts</h1>
-        <Filter filterContact={this.filterContact}></Filter>
-        <ContactList
-          contacts={this.state.contacts}
-          filter={this.state.filter}
-          deleteContact={this.handleDeleteContacts}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <ContactForm saveContact={saveContact}></ContactForm>
+      <h1>Contacts</h1>
+      <Filter filterContact={filterContact}></Filter>
+      <ContactList
+        contacts={contacts}
+        filter={filter}
+        deleteContact={handleDeleteContacts}
+      />
+    </>
+  );
+};
 
 export default App;
